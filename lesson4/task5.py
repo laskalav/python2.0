4d60dae8ad7bf38f8b8dee7b0825e4c383f65638
LaskaLavr <laskalav@gmail.com>
2021-03-13 21:47:21 +0300

    Lessons

--- lesson4/task5.py
+++ lesson4/task5.py
@@ -4,4 +4,4 @@ from mymodule import get_currency_rate
 for el in argv[1:]:
     print(get_currency_rate(el))
 
-
+print([(get_currency_rate(el)) for el in argv[1:]])
---
+++ lesson5/task1.py
@@ -0,0 +1,41 @@
+"""
+Задание 1. Функция-генератор rand_nums
+
+Написать функцию-генератор rand_nums, генерирующую N случайных целых чисел в диапазоне 1 до 100 (включительно).
+Количество чисел N, которое выдаст генератор передается в функцию через параметр:
+
+# >>> rand15 = rand_nums(15)
+# >>> next(rand15) # 1-й вызов
+11
+# >>> next(rand15) # 2-й вызов
+38
+...
+# >>> next(rand15) # 15-й вызов
+7
+# >>> next(rand15) # 16-й вызов
+# ...StopIteration...
+"""
+from random import randint
+
+
+def rand_nums(limit):
+    counter = 1
+    while counter <= limit:
+        rand_number = randint(1, 100)
+        yield rand_number
+        counter += 1
+
+
+rand = rand_nums(2)
+try:
+    print(next(rand))
+    print(next(rand))
+    print(next(rand))
+    print(next(rand))
+    print(next(rand))
+    print(next(rand))
+except StopIteration:
+    print("...StopIteration...")
+
+
+
